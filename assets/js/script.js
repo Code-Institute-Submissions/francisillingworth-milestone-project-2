32aac0066abe765ebfb676d5ae0837fe502cb87f
Francis Illingworth <francis.illingworth@gmail.com>
2019-05-30 15:23:03 +0000

    minor style adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#button_effects1").click(function(){
         $('#par1').toggle('1000');
+        
     });
     $("#button_effects2").click(function(){
         $('#par2').toggle('1000');
@@ -8,5 +9,38 @@ $(document).ready(function() {
     $("#button_effects3").click(function(){
         $('#par3').toggle('1000');
     });
-
+    $("#button_effects1").mouseenter(function(){
+        $(this).addClass("highlight");
+    });
+     $("#button_effects1").mouseleave(function(){
+        $(this).removeClass("highlight");
+    });
+     $("#button_effects2").mouseenter(function(){
+        $(this).addClass("highlight");
+    });
+    
+     $("#button_effects2").mouseleave(function(){
+        $(this).removeClass("highlight");
+    });
+     $("#button_effects3").mouseenter(function(){
+        $(this).addClass("highlight");
+    });
+     $("#button_effects3").mouseleave(function(){
+        $(this).removeClass("highlight");
+    });
+    
 }); 
+
+
+
+$('.SeeMore2').click(function(){
+    var $this = $(this);
+    $this.toggleClass('SeeMore2');
+    if($this.hasClass('SeeMore2')){
+        $this.text('See More');         
+    } else {
+        $this.text('See Less');
+    }
+});
+
+
